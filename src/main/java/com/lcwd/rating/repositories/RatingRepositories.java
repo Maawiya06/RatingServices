3fda4b288004ead112092e3eaf16e80e7256e6e0
Maawiya06 <maawiyaameer001@gmail.com>
2026-01-12 15:12:44 +0530

    commit

--- src/main/java/com/lcwd/rating/repositories/RatingRepositories.java
+++ src/main/java/com/lcwd/rating/repositories/RatingRepositories.java
@@ -3,5 +3,11 @@ package com.lcwd.rating.repositories;
 import com.lcwd.rating.entities.Rating;
 import org.springframework.data.mongodb.repository.MongoRepository;
 
+import java.util.List;
+
 public interface RatingRepositories extends MongoRepository<Rating, String> {
+
+    // custom finder method
+    List<Rating> findByUserId(String userId);
+    List<Rating> findByHotelId(String hotelId);
 }
---
+++ src/main/java/com/lcwd/rating/services/Implementation/RatingServicesImplementation.java
@@ -0,0 +1,35 @@
+package com.lcwd.rating.services.Implementation;
+
+import com.lcwd.rating.entities.Rating;
+import com.lcwd.rating.repositories.RatingRepositories;
+import com.lcwd.rating.services.RatingServices;
+import org.springframework.beans.factory.annotation.Autowired;
+
+import java.util.List;
+
+public class RatingServicesImplementation implements RatingServices {
+
+    @Autowired
+    private RatingRepositories ratingRepositories;
+
+
+    @Override
+    public Rating create(Rating rating) {
+        return ratingRepositories.save(rating);
+    }
+
+    @Override
+    public List<Rating> getAllRating() {
+        return ratingRepositories.findAll();
+    }
+
+    @Override
+    public List<Rating> getRatingByUserId(String userId) {
+        return List.of();
+    }
+
+    @Override
+    public List<Rating> getRatingByHotelId(String hotelId) {
+        return List.of();
+    }
+}
